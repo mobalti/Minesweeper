2a19b4c120f9a74d3f2e32be48da87149ad7cb5a
Mo Balić <65825731+mobalic@users.noreply.github.com>
2022-04-24 18:46:59 +0200

    Add files via upload

---
+++ minesweeper/Main.java
@@ -0,0 +1,9 @@
+package minesweeper;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Game board = new Game();
+        board.playGame();
+    }
+}
---
+++ minesweeper/State.java
@@ -0,0 +1,7 @@
+package minesweeper;
+
+public enum State {
+    NON_MARKED,
+    WITH_NUMBER,
+    MARKED_CELL,
+}
